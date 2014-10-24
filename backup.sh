720eb8fc02a5ab27d45edd90cccf099b3f85dca4
Wferr <info@wferr.com>
2014-10-23 18:22:32 -0700

    added some info about backup

--- backup.sh
+++ backup.sh
@@ -11,6 +11,8 @@ FILE="$NOW.tar.gz"
 
 #Get into Directory to GZIP up!
 #And also Put the GZIP info /backups
+#To run daily use "0 5 * * * /home/backup.sh" as a cron job 
+#Runs at 5am every day
 tar cvzf /home/backups/$NOW.tar.gz /home/minecraft
 
 echo "Done!"
